192c293857b7177087c3c9f50837536cb65637fe
Sujeet Jaiswara <sujeet@cleartrust.cc>
2024-12-10 20:47:37 +0530

    Added - svelte:boundary tag for global error handling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,49 +14,74 @@
 	}
 
 	let { data, children }: Props = $props();
+
+	let error = $state(null);
+	let reset = $state(() => {});
+
+	function onerror(e: any, r: any) {
+		error = e;
+		reset = r;
+	}
 </script>
 
-<div class="border-b py-[6px]">
-	<div class="container flex justify-between">
-		<a href="/" class="flex items-center text-gray-700">
-			<img src="/logo.png" alt="" class="h-8 w-8" />
-			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-				Popcorn Pluse
-			</span>
-		</a>
-		<div class="flex items-center space-x-2">
-			<Button
-				variant="link"
-				size="icon"
-				href="https://github.com/sujeetjaiswara/popcorn-pluse-svelte"
-				target="_blank"
-			>
-				<Github class="h-[1.2rem] w-[1.2rem]" />
-			</Button>
-			<Button variant="ghost" size="icon" onclick={toggleMode}>
-				<Sun
-					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-				/>
-				<Moon
-					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-				/>
-				<span class="sr-only">Toggle theme</span>
-			</Button>
+<svelte:boundary {onerror}>
+	<ModeWatcher />
+
+	<div class="border-b py-[6px]">
+		<div class="container flex justify-between">
+			<a href="/" class="flex items-center text-gray-700">
+				<img src="/logo.png" alt="" class="h-8 w-8" />
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+					Popcorn Pluse
+				</span>
+			</a>
+			<div class="flex items-center space-x-2">
+				<Button
+					variant="link"
+					size="icon"
+					href="https://github.com/sujeetjaiswara/popcorn-pluse-svelte"
+					target="_blank"
+				>
+					<Github class="h-[1.2rem] w-[1.2rem]" />
+				</Button>
+				<Button variant="ghost" size="icon" onclick={toggleMode}>
+					<Sun
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Moon
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+					<span class="sr-only">Toggle theme</span>
+				</Button>
+			</div>
 		</div>
 	</div>
-</div>
 
-<!-- Page container -->
-{#key data.url}
-	<ScrollArea class="h-[calc(100vh-3.8rem)]">
-		<div
-			class="container mx-auto mt-3"
-			in:fly={{ x: -50, duration: 100, delay: 100 }}
-			out:fly={{ x: 50, duration: 100 }}
+	<!-- Page container -->
+	{#key data.url}
+		<ScrollArea class="h-[calc(100vh-3.8rem)]">
+			<div
+				class="container mx-auto mt-3"
+				in:fly={{ x: -50, duration: 100, delay: 100 }}
+				out:fly={{ x: 50, duration: 100 }}
+			>
+				{@render children?.()}
+			</div>
+		</ScrollArea>
+	{/key}
+</svelte:boundary>
+
+{#if error}
+	<div class="container mx-auto mt-3 flex flex-col items-center justify-center gap-y-3">
+		<pre class="text-base text-red-800">{error}</pre>
+		<Button
+			variant="destructive"
+			onclick={() => {
+				error = null;
+				reset();
+			}}
 		>
-			{@render children?.()}
-		</div>
-	</ScrollArea>
-{/key}
-
-<ModeWatcher />
+			oops! try again
+		</Button>
+	</div>
+{/if}
